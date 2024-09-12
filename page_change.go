5963d9078a224cac22f9efe042c67cd359f14be0
Ricardo Esquito <resquito@wikimedia.org>
2024-09-12 13:35:36 +0100

    Update PageChange model

--- page_change.go
+++ page_change.go
@@ -7,25 +7,36 @@ import (
 
 // PageChange event scheme struct
 type PageChange struct {
-	baseSchema
+	ID   []Info
 	Data struct {
-		baseData
+		Schema    string `json:"$schema"`
+		Meta      Meta   `json:"meta"`
+		Performer struct {
+			UserText           string    `json:"user_text"`
+			UserGroups         []string  `json:"groups"`
+			UserIsBot          bool      `json:"is_bot"`
+			UserID             int       `json:"user_id"`
+			UserRegistrationDt time.Time `json:"registration_dt"`
+			UserEditCount      int       `json:"edit_count"`
+		} `json:"performer"`
 		Dt             time.Time `json:"dt"`
 		ChangelogKind  string    `json:"changelog_kind"`
 		PageChangeKind string    `json:"page_change_kind"`
 		Page           struct {
 			PageID         int    `json:"page_id"`
 			PageTitle      string `json:"page_title"`
-			PageNamespace  int    `json:"page_namespace"`
-			PageIsRedirect bool   `json:"page_is_redirect"`
+			PageNamespace  int    `json:"namespace_id"`
+			PageIsRedirect bool   `json:"is_redirect"`
 		} `json:"page"`
 		Revision struct {
 			RevID        int       `json:"rev_id"`
 			RevDt        time.Time `json:"rev_dt"`
 			Comment      string    `json:"comment"`
 			ContentSlots struct {
-				ContentFormat string `json:"content_format"`
-				ContentModel  string `json:"content_model"`
+				Main struct {
+					ContentFormat string `json:"content_format"`
+					ContentModel  string `json:"content_model"`
+				} `json:"main"`
 			} `json:"content_slots"`
 			IsCommentVisible bool   `json:"is_comment_visible"`
 			IsContentVisible bool   `json:"is_content_visible"`
@@ -35,6 +46,12 @@ type PageChange struct {
 			RevSha1          string `json:"rev_sha1"`
 			RevSize          int    `json:"rev_size"`
 		} `json:"revision"`
+		PriorState struct {
+			Page struct {
+				PageTitle     string `json:"page_title"`
+				PageNamespace int    `json:"namespace_id"`
+			} `json:"page"`
+		} `json:"prior_state"`
 		Database string `json:"wiki_id"`
 	}
 }
